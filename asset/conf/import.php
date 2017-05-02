e4b91e5b1582b63779ac44b342544961a8babad2
umarfaisol <umarfaisol@um.ac.id>
2017-05-02 11:40:23 +0700

    ubah conf main

--- asset/conf/import.php
+++ asset/conf/import.php
@@ -1,13 +1,5 @@
 <?php
-/*String db = request.getServletContext().getRealPath("/asset/db/rt02.accdb");*/
-/*error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);*/
-error_reporting(E_ERROR);
-$GLOBALS["ROOT"] = "/home/u837302425/public_html";
-$GLOBALS["SERVER"] = "http://rt02rw14.pe.hu";
-
-$www = array();
-$www["ROOT"] = $GLOBALS["ROOT"];
-$www["SERVER"] = $GLOBALS["SERVER"];
+include("main.php");
 
 $sqlitedb = "/asset/db/rt02.db";
 include($www["ROOT"]."/asset/classes/SqliteCon.php");
---
+++ asset/conf/main.php
@@ -0,0 +1,13 @@
+<?php
+error_reporting(E_ERROR);
+/*
+$GLOBALS["ROOT"] = "/home/u837302425/public_html";
+$GLOBALS["SERVER"] = "http://rt02rw14.pe.hu";
+*/
+$GLOBALS["ROOT"] = "D:/DATA/UMAR/Proyek/RT02/RT02-php";
+$GLOBALS["SERVER"] = "http://rt02rw14.local";
+
+$www = array();
+$www["ROOT"] = $GLOBALS["ROOT"];
+$www["SERVER"] = $GLOBALS["SERVER"];
+?>
